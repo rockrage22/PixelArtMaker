a70e53126df37966a7909e355b06a99d6afc73ca
Raquel <100424496+rockrage22@users.noreply.github.com>
2022-05-05 09:23:22 -0500

    Corrections

--- designs.js
+++ designs.js
@@ -1,28 +1,28 @@
 // Select color input
 // Select size input
-var height, color, width;
+var height, width, color;
 // When size is submitted by the user, call makeGrid()
-$('#sizepicker').submit(function (event) {
+$('#sizePicker').submit(function (event) {
   event.preventDefault();
-  heighgt = $('#inputheight').val();
+  height = $('#inputheight').val();
   width = $('#inputweight').val();
   makeGrid(height, width);
 })
 
 
 function makeGrid(x, y) {
-  s('tr').remove();
+  $('tr').remove();
 // Your code goes here!
   for (var i = 1; i <= x; i++) {
-    $('pixelCanvas').append('<tr id=table' + i + '></tr>');
+    $('#pixelCanvas').append('<tr id=table' + i + '></tr>');
     for (var j = 1; j <= y; j++) {
       $('#table' + i).append('<td></td>');
     }
   }
   
-  // color to the cell
+  // color the  cell
   $('td').click(function addColor() {
-    color = $('#colorPicker'),val();
+    color = $('#colorPicker').val();
     
     if ($(this).attr('style')) {
       $(this).removeAttr('style')
