fe3232baf3b8e3c83303cb780f282f55a3acc8c4
Raquel <100424496+rockrage22@users.noreply.github.com>
2022-05-13 12:11:09 -0500

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,10 +1,9 @@
 // Select color input
+var colorPicker = document.getElementById('colorPicker');
+
 // Select size input
 
+
 // When size is submitted by the user, call makeGrid()
-
 function makeGrid() {
-
 // Your code goes here!
-
-}
