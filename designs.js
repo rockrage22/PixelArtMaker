472ce154fed4e1e8292c8a786e6f584749b84465
Raquel <100424496+rockrage22@users.noreply.github.com>
2022-05-23 06:07:40 -0500

    codepen

--- designs.js
+++ designs.js
@@ -1,9 +1,55 @@
 // Select color input
-var colorPicker = document.getElementById('colorPicker');
-
+const sizePicker = $('#sizePicker'),
+      inputHeight = $('#inputHeight'),
+      inputWidth = $('#inputWidth'),
 // Select size input
-
+      colorPicker = $('#colorPicker'),
+      pixelCanvas = $('#pixelCanvas');
+let color = 'black';
 
 // When size is submitted by the user, call makeGrid()
-function makeGrid() {
-// Your code goes here!
+const sizePicker = $('#sizePicker'),
+      inputHeight = $('#inputHeight'),
+      inputWidth = $('#inputWidth'),
+      // Select size input
+      colorPicker = $('#colorPicker'),
+      pixelCanvas = $('#pixelCanvas');
+let color = 'black';
+
+
+let makeGrid = (row, column) => {
+  let html;
+  for (let r = 0; r < row; r++) {
+    html += `<tr>`;
+    for (var c = 0; c < column; c++) {
+      html += `<td class="pixel"></td>`
+    }
+    html += `</tr>`;
+  }
+  pixelCanvas.append(html);
+}
+
+
+let deleteRow = () => {
+  let rowCounter = $('tr').length;
+  while (rowCounter--) {
+    document.getElementById('pixelCanvas').deleteRow(-1);
+  }
+}
+
+colorPicker.change(() => {
+  color = colorPicker.val();
+});
+
+
+sizePicker.submit(function(e) {
+  e.preventDefault();
+  let row = inputHeight.val(),
+    column = inputWidth.val(),
+    trs = $('tr').length;
+  deleteRow();
+  makeGrid(row, column);
+  $('td').click(function() {
+    $(this).css('background-color', color);
+  })
+});
